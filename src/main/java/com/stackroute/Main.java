4ea9e2cffc12c41fba486e7c29a2f4f58189b443
Sabya7 <sabya62.ss@gmail.com>
2019-07-11 15:16:34 +0530

    Task 1

---
+++ src/main/java/com/stackroute/Main.java
@@ -0,0 +1,40 @@
+package com.stackroute;
+
+import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+       Movie mv= context.getBean("movie",Movie.class);
+       System.out.println(mv);
+
+        @Deprecated
+        BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        @Deprecated
+        Movie moviev= (Movie) beanFactory.getBean("movie");
+         System.out.println(moviev);
+
+        /*BeanDefinitionRegistry beanDefinitionRegistry=new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        BeanFactory beanFactory1=new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        BeanDefinitionReader beanDefinitionReader=new XmlBeanDefinitionReader(beanFactory1);
+        System.out.println(beanDefinitionReader.loadBeanDefinitions("beans.xml"));
+      */
+        @Deprecated
+        BeanDefinitionRegistry factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        XmlBeanDefinitionReader rdr = new XmlBeanDefinitionReader(factory);
+       // rdr.loadBeanDefinitions(new ClassPathResource("beans.xml"));
+        Movie mv1 =((XmlBeanFactory)factory).getBean("movie",Movie.class);
+        System.out.println(mv1);
+    }
+}
